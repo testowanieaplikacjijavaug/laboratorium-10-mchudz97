9eb19cd629e6c291789aaf311dfef79931909f5a
Maciej Chudzinski <mchudz97@gmail.com>
2020-04-27 10:20:21 +0200

    zad 1 headless xd

--- src/test/java/zad1/FindTest.java
+++ src/test/java/zad1/FindTest.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FindTest {
@@ -24,7 +25,13 @@ public class FindTest {
     @BeforeAll
     public static void setUpDriver(){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        driver = new ChromeDriver();
+
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        chromeOptions.addArguments("--headless");
+
+        driver = new ChromeDriver(chromeOptions);
         // Implicity wait -> max czas na znalezienie elementu na stronie
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
